0a614a12470ac41d1a489ce4a892bdbc50ffa21a
Chris Polishak <47062141+cpolishak@users.noreply.github.com>
2019-09-20 16:58:04 -0500

    Initial setup to build upon
    Want to build this app for eventual deployment with heroku

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
+
+
+
+app.listen(PORT, function () {
+    console.log('Example app listening on port 5000!');
+   });
