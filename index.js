5f63e2be18e48581efeb77d15459ab7a655f15db
Chris Polishak <47062141+cpolishak@users.noreply.github.com>
2019-09-26 20:47:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.get("/", function (req, res) {
-    res.send("Hello World. Testing if this deployment works");
+    res.send("Hello World");
 });
 
 
